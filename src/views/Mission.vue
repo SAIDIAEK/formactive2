5b8c60e87f5ebf5c3d8cb2032e15c9e571a31d30
SAIDI Abdelkader <a_saidi@esi.dz>
2024-05-25 09:01:56 +0100

    Modification page mission

--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -1,30 +1,50 @@
 <template>
   <div>
-    <div class="text-3xl font-bold text-center my-10">Our mission</div>
-
-    <p>
-      Notre groupement d'entreprises, implanté au niveau du Pôle technologique du Cyberparc de Sidi Abdallah, est spécialisé
-dans le secteur de la formation visant le développement des compétences, leur qualification et certification en milieu
-professionnel.
-Conscient que la ressource humaine est un facteur clé de succès pour toute entreprise, nous avons conçu, en vue de
-vous permettre d'atteindre tout niveau d'efficacité et d'efficience escompté, un service centrique porté sur :
-• Une approche professionnelle et personnalisée ;
-• Une analyse des besoins de formation ;
-• Des plans de formation adaptés aux attentes des entreprises ;
-• Une flexibilité dans la réalisation des projets de formation (en ligne/présentiel, dans nos infrastructures/sur site,
-continue/modulée ou alternée…etc.) ;
-• Un champ d’expertise multidisciplinaire ;
-• Des coûts étudiés.
-Les formations que nous vous proposons visent à développer de nouvelles compétences et renforcer les acquis de vos
-collaborateurs, les familiariser avec les dernières pratiques et innovations dans leur domaine d'activité.
-Nous sommes conscients que vos besoins en formation peuvent s'étendre au-delà des domaines proposés. C'est
-pourquoi notre Groupement est à votre disposition pour analyser vos besoins spécifiques et vous proposer des solutions
-sur mesure, participer à l’élaboration de programmes de formation personnalisés en collaboration avec vos équipes et
-adapter nos formations aux contraintes spécifiques de votre secteur d'activité. Nous restons à votre entière disposition
-pour toute information complémentaire.
-En vous remerciant de votre confiance, nous vous prions d'agréer l'expression de nos
-salutations distinguées.
-    </p>
+    <div class="text-3xl font-bold text-center my-10">Notre mission</div>
+    <div class="container mx-auto px-4 text-lg leading-relaxed text-justify font-medium text-gray-900">
+      <p class="indent-6 mb-4">
+        Notre groupement d'entreprises, implanté au niveau du Pôle technologique
+        du Cyberparc de Sidi Abdallah, est spécialisé dans le secteur de la
+        formation visant le développement des compétences, leur qualification et
+        certification en milieu professionnel.
+      </p>
+      <p class="indent-6 mb-4">
+        Conscient que la ressource humaine est un facteur clé de succès pour
+        toute entreprise, nous avons conçu, en vue de vous permettre d'atteindre
+        tout niveau d'efficacité et d'efficience escompté, un service centrique
+        porté sur :
+      </p>
+      <ul class="list-disc list-inside mb-4 space-y-1 ml-12">
+      <!-- <ul class="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400"> -->
+        <li>Une approche professionnelle et personnalisée ;</li>
+        <li>Une analyse des besoins de formation ;</li>
+        <li>Des plans de formation adaptés aux attentes des entreprises ;</li>
+        <li>
+          Une flexibilité dans la réalisation des projets de formation (en
+          ligne/présentiel, dans nos infrastructures/sur site, continue/modulée
+          ou alternée…etc.) ;
+        </li>
+        <li>Un champ d’expertise multidisciplinaire ;</li>
+        <li>Des coûts étudiés.</li>
+      </ul>
+      <p class="indent-6 mb-4">
+        Les formations que nous vous proposons visent à développer de nouvelles
+        compétences et renforcer les acquis de vos collaborateurs, les
+        familiariser avec les dernières pratiques et innovations dans leur
+        domaine d'activité.
+      </p>
+      <p class="indent-6 mb-4">
+        Nous sommes conscients que vos besoins en formation peuvent s'étendre
+        au-delà des domaines proposés. C'est pourquoi notre Groupement est à
+        votre disposition pour analyser vos besoins spécifiques et vous proposer
+        des solutions sur mesure, participer à l’élaboration de programmes de
+        formation personnalisés en collaboration avec vos équipes et adapter nos
+        formations aux contraintes spécifiques de votre secteur d'activité. Nous
+        restons à votre entière disposition pour toute information
+        complémentaire. En vous remerciant de votre confiance, nous vous prions
+        d'agréer l'expression de nos salutations distinguées.
+      </p>
+    </div>
   </div>
 </template>
 
