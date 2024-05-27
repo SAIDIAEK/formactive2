cb7b4e71d84e84d3cd15409ee86fe20c906afd6c
SAIDI Abdelkader <a_saidi@esi.dz>
2024-05-27 17:28:59 +0100

    Correction Navbar3

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,9 +7,9 @@
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Formactive Center</span>
       </router-link>
       
-        <button data-collapse-toggle="navbar-multi-level" type="button" @click="toggleMenu"
+        <button  type="button" @click="toggleMenu"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-multi-level" :aria-expanded="isMenuOpen.toString()">
+          aria-controls="navbar-multi-level" :aria-expanded="isMenuOpen"> <!-- data-collapse-toggle="navbar-multi-level"  -->
           <span class="sr-only">Open main menu</span>
           <svg class="w-5 h-5" aria-hidden="true" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -20,7 +20,7 @@
           <ul
             class="flex flex-col font-medium p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Navlink name="home" title="Home" @click="toggleMenu" />
+              <Navlink name="home" title="Home" @click="handleMenuClick" />
             </li>
             <li>
               <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
@@ -107,23 +107,25 @@ const isMenuOpen  = ref(false);
 const menuContainer = ref(null);
 
 const toggleMenu = () => {
-  isMenuOpen.value = !isMenuOpen.value;
+    isMenuOpen.value = !isMenuOpen.value;
+    console.log(isMenuOpen.value)
 };
 
 const closeMenu = () => {
-  isMenuOpen.value = false;
+    isMenuOpen.value = false;
 };
 
 const handleMenuClick = () => {
   if (window.innerWidth < 768) {
     closeMenu();
+    console.log(isMenuOpen.value)
   }
 };
 
 const handleClickOutside = (event) => {
-  console.log(window.innerWidth)
   if ((menuContainer.value && !menuContainer.value.contains(event.target)) && isMenuOpen ) {
     closeMenu();
+    console.log(isMenuOpen.value)
   }
 };
 
--- src/components/Navlink.vue
+++ src/components/Navlink.vue
@@ -21,7 +21,7 @@ const { name, title} = defineProps({
 const route = useRoute();
 const classes = computed(() =>
     route.name === name
-        ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent' 
+        ? 'text-white bg-blue-700 font-bold md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent' 
         : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
 );
 </script>
