ea99b12e8b4c8e3a28f64d6dfce1826b0c3f88db
SAIDI Abdelkader <a_saidi@esi.dz>
2024-06-02 12:36:39 +0100

    Modify contact page (verifiction of email structure)

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex items-center justify-center min-h-screen bg-gray-100 p-6">
     <div class="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
-      <h2 class="text-2xl font-bold mb-6">Contact Us</h2>
+      <h2 class="text-2xl font-bold mb-6">Contacter Nous</h2>
       <form @submit.prevent="sendEmail">
         <div class="mb-4">
-          <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+          <label for="name" class="block text-sm font-medium text-gray-700">Nom</label>
           <input type="text" v-model="form.name" id="name" required class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm">
         </div>
         <div class="mb-4">
@@ -33,27 +33,45 @@ export default {
       name: '',
       email: '',
       message: '',
-      to_name: 'SAIDI Abdelkader'
     });
     const status = ref(false);
     const statusMessage = ref('');
     const statusClass = ref('');
 
+    const validateEmail = (email) => {
+      //const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      const re = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+      return re.test(email);
+    };
+
     const sendEmail = () => {
+      if (!validateEmail(form.value.email)) {
+        status.value = true;
+        statusMessage.value = 'S\'il vous plaît, mettez une adresse email valide.';
+        statusClass.value = 'text-red-500';
+        return;
+      }
+
+      const templateParams = {
+        name: form.value.name,
+        from_email: form.value.email,
+        message: form.value.message,
+        to_name: 'Formactive Center', // Replace with your recipient name if needed
+      };
       emailjs.send(
         'service_formatec', // Replace with your service ID
         'template_formatec', // Replace with your template ID
-        form.value,
+        templateParams,
         'Ptc6WiOwNucYepW7Q' // Replace with your Public Key
       )
       .then((response) => {
         status.value = true;
-        statusMessage.value = 'Message sent successfully!';
+        statusMessage.value = 'Message envoyé avec succès!';
         statusClass.value = 'text-green-500';
         resetForm();
       }, (error) => {
         status.value = true;
-        statusMessage.value = 'Failed to send message. Please try again.';
+        statusMessage.value = 'Échec d\'envoi du message. Veuillez réessayer.';
         statusClass.value = 'text-red-500';
       });
     };
@@ -70,5 +88,13 @@ export default {
 </script>
 
 <style scoped>
-/* Add any additional styling if needed */
+@keyframes blink {
+  50% {
+    opacity: 0;
+  }
+}
+
+.cursor {
+  animation: blink 1s step-start infinite;
+}
 </style>
